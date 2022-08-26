d1f349996e0d24f4a833c6eebc414227d8e8fcc0
Goverdhan Gaur <goverdhangaur02@gmail.com>
2022-08-26 03:11:35 -0400

    Init boilerplate

---
+++ src/components/Homepage/Homepage.stories.jsx
@@ -0,0 +1,27 @@
+import Homepage from './Homepage';
+
+//👇 This default export determines where your story goes in the story list
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    /* 👇 The title prop is optional.
+    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+    * to learn how to generate automatic titles
+    */
+    title: 'Homepage',
+    component: Homepage,
+  };
+  
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args) => <Homepage {...args} />;
+
+export const Normal = {
+  args: {
+    //👇 The args you need here will depend on your component
+    
+  },
+  argTypes : {
+    //👇 The argTypes you need here will depend on your component
+    
+  }
+};
